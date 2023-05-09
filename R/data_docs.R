8f79e5a8ad7a6bc0abab539abf26ec1edea73c26
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-05-09 14:52:47 +0100

    add dependency to mschart & suggest officer add data object & docs

---
+++ R/data_docs.R
@@ -0,0 +1,24 @@
+#' DisplayR example
+#'
+#' Data frame with some useful variables for demonstration purposes/
+#'
+#' @format A data frame with 5,000 observations of 15 variables.
+#' \describe{
+#' \item{document}{A document id}
+#' \item{data}{placeholder}
+#' \item{text_copy}{placeholder}
+#' \item{text}{placeholder}
+#' \item{topic}{placeholder}
+#' \item{permalink}{placeholder}
+#' \item{cluster}{placeholder}
+#' \item{sentiment}{placeholder}
+#' \item{V1}{placeholder}
+#' \item{V2}{placeholder}
+#' \item{new_clusters}{placeholder}
+#' \item{universal_message_id}{placeholder}
+#' \item{fifty_clusters}{placeholder}
+#' \item{topic_n}{placeholder}
+#' \item{sender_screen_name}{placeholder}
+#' }
+#' @export
+"disp_example"
