b7193d93f95f70619ccdd1b8dc0a39f188a181ae
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-05-23 19:09:28 +0100

    expand plots_volume tests

--- tests/testthat/test-plots_volume.R
+++ tests/testthat/test-plots_volume.R
@@ -1,3 +1,4 @@
+#These tests are largely unit tests, failures should indicate that either the example data has changed, or changes to the functions have resulted in differing functionality.
 test_that("dr_vot_plots input variables raise errors when they should and don't when they shouldn't and lines are default geom", {
 
   #Get some data for plotting
@@ -35,5 +36,28 @@ test_that("dr_plot_vot bars are working", {
 
 })
 
+test_that("dr_plot_vot_group fails and succeeds when it ought to", {
+  plot <- DisplayR::disp_example %>%
+    dr_plot_vot_group(group_var = topic, date_var = date, time_unit = "day")
+  expect_s3_class(plot, "ggplot")
 
+  #Check the first value of n == 6,if not function behaviour or example data has changed
+  expect_true(plot$data[["n"]][[1]] == 6)
 
+  #Function takes symbol and string for group and date_var arguments
+  plot_two <- DisplayR::disp_example %>%
+    dr_plot_vot_group(group_var = "topic", date_var = "date", time_unit = "day")
+  expect_s3_class(plot_two, "ggplot")
+
+  #Check this doesn't change behaviourl and that no stochasticity in plot (why would there be...?)
+  expect_true(all(plot_two$data$n == plot$data$n))
+
+  #Check errors are raised when they ought to be
+  expect_error(DisplayR::disp_example %>%
+                 dr_plot_vot_group(group_var = topic, date_var = date, time_unit = "second"))
+  expect_error(DisplayR::disp_example %>%
+                 dr_plot_vot_group(group_var = topic2, date_var = date, time_unit = "day"))
+  expect_error(DisplayR::disp_example %>%
+                 dr_plot_vot_group(group_var = topic, date_var = date2, time_unit = "month"))
+
+})
