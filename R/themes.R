8522bd349bda352a73f025a701f4c3f8714228d6
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-05-24 15:53:51 +0100

    Add to globals, tidy up color -> colour Add base of theme vignette (much work needed)

--- R/themes.R
+++ R/themes.R
@@ -4,18 +4,16 @@
 #' @export
 dr_theme_black <- function(){
 
-  output <- ggplot2::theme(plot.background = ggplot2::element_rect(fill = "black", color = "black"),
-                          strip.text = ggplot2::element_text(color = "white"),
+  output <- ggplot2::theme(plot.background = ggplot2::element_rect(fill = "black", colour = "black"),
+                          strip.text = ggplot2::element_text(colour = "white"),
                           strip.background = ggplot2::element_rect(fill = "black"),
                           panel.background = ggplot2::element_rect(fill = 'black'),
-                          axis.text.x = ggplot2::element_text(color = 'white'),
-                          axis.title.x = ggplot2::element_text(color = 'white'),
-                          axis.text.y = ggplot2::element_text(color = 'white'),
-                          axis.line.y = ggplot2::element_line(color = 'white'),
-                          axis.title.y = ggplot2::element_text(color = "white"),
-                          legend.text = ggplot2::element_text(color = "white"),
+                          axis.text = ggplot2::element_text(colour = 'white'),
+                          axis.title = ggplot2::element_text(colour = 'white'),
+                          axis.line = ggplot2::element_line(colour = 'white'),
+                          legend.text = ggplot2::element_text(colour = "white"),
                           legend.background = ggplot2::element_rect(fill = 'black'),
-                          legend.title = ggplot2::element_text(color = 'white'),
+                          legend.title = ggplot2::element_text(colour = 'white'),
                           panel.grid.major = ggplot2::element_blank(),
                           panel.grid.minor = ggplot2::element_blank())
 
@@ -29,7 +27,7 @@ dr_theme_black <- function(){
 #' The user can specify whether they want a continuous or discrete theme by providing the `scale_type` argument.
 #'
 #' @param scale_type A character string specifying the type of scale, either "continuous" or "discrete". Default is "discrete".
-#' @param index An optional numeric index or vector of indices to select specific colours from the Microsoft color palette.
+#' @param index An optional numeric index or vector of indices to select specific colours from the Microsoft colour palette.
 #' @param direction An optional numeric value (1 or -1) specifying the direction of the colour gradient for continuous scales. Default is 1.
 #' @param guide An optional character string specifying the type of guide to use for continuous scales. Default is 'legend'.
 #' @param fallback_font Adds a fallback font of 'sans' in case user does not have required font.
@@ -42,12 +40,12 @@ dr_theme_black <- function(){
 #' library(ggplot2)
 #'
 #' # Example with continuous theme
-#' ggplot(mtcars, aes(x = wt, y = mpg, color = qsec)) +
+#' ggplot(mtcars, aes(x = wt, y = mpg, colour = qsec)) +
 #'   geom_point() +
 #'   theme_microsoft(scale_type = "continuous")
 #'
 #' # Example with discrete theme
-#' ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+#' ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, colour = Species)) +
 #'   geom_point() +
 #'   theme_microsoft(scale_type = "discrete")
 #'   }
@@ -187,16 +185,16 @@ theme_microsoft_discrete <- function(index = NULL, font_family = 'Segoe UI Regul
 
 #' Apply SAMY Themed Colour Scales and Aesthetics to ggplot2 Plots
 #'
-#' This function provides a convenient way to apply Microsoft themed color scales and aesthetics to ggplot2 plots.
+#' This function provides a convenient way to apply Microsoft themed colour scales and aesthetics to ggplot2 plots.
 #' The user can specify whether they want a continuous or discrete theme by providing the `scale_type` argument.
 #'
 #' @param scale_type A character string specifying the type of scale, either "continuous" or "discrete". Default is "discrete".
-#' @param index An optional numeric index or vector of indices to select specific colors from the SAMY color palette.
-#' @param direction An optional numeric value (1 or -1) specifying the direction of the color gradient for continuous scales. Default is 1.
+#' @param index An optional numeric index or vector of indices to select specific colours from the SAMY colour palette.
+#' @param direction An optional numeric value (1 or -1) specifying the direction of the colour gradient for continuous scales. Default is 1.
 #' @param guide An optional character string specifying the type of guide to use for continuous scales. Default is 'legend'.
 #' @param fallback_font Adds a fallback font of 'sans' in case user does not have required font.
 #'
-#' @return A list containing the ggplot2 theme, fill scale, and color scale.
+#' @return A list containing the ggplot2 theme, fill scale, and colour scale.
 #' @export
 #'
 #' @examples
@@ -205,12 +203,12 @@ theme_microsoft_discrete <- function(index = NULL, font_family = 'Segoe UI Regul
 #' library(ggplot2)
 #'
 #' # Example with continuous theme
-#' ggplot(mtcars, aes(x = wt, y = mpg, color = qsec)) +
+#' ggplot(mtcars, aes(x = wt, y = mpg, colour = qsec)) +
 #'   geom_point() +
 #'   theme_samy(scale_type = "continuous")
 #'
 #' # Example with discrete theme
-#' ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+#' ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, colour = Species)) +
 #'   geom_point() +
 #'   theme_samy(scale_type = "discrete")
 #'}
@@ -221,7 +219,7 @@ dr_theme_samy <- function(scale_type = c("discrete", "continuous"),
                        fallback_font = "sans"){
   scale_type <- match.arg(scale_type)
 
-  if (identical(Sys.getenv("R_CHECK_ENVIRON"), "true")) {
+  if (identical(Sys.getenv("R_CHECK_ENVIRON"), "true") || !'Montserrat Regular' %in% names(pdfFonts())) {
     font_family <- fallback_font
   } else {
     font_family <- 'Montserrat Regular'
@@ -320,6 +318,148 @@ theme_samy_discrete <- function(index = NULL, font_family = 'Montserrat Regular'
                        "#aaabaa",
                        "#f8aa95",
                        "#dfe0e1")
+
+  if (is.numeric(index)){
+    values <- values[index]
+  }
+
+  list(ggplot2::theme_minimal(base_family = font_family),
+       ggplot2::scale_discrete_manual(aesthetics = c('fill', 'colour'),
+                                      values = values))
+
+}
+
+
+#' Apply share Themed colour Scales and Aesthetics to ggplot2 Plots
+#'
+#' This function provides a convenient way to apply share themed colour scales and aesthetics to ggplot2 plots.
+#' The user can specify whether they want a continuous or discrete theme by providing the `scale_type` argument.
+#'
+#' @param scale_type A character string specifying the type of scale, either "continuous" or "discrete". Default is "discrete".
+#' @param index An optional numeric index or vector of indices to select specific colours from the share colour palette.
+#' @param direction An optional numeric value (1 or -1) specifying the direction of the colour gradient for continuous scales. Default is 1.
+#' @param guide An optional character string specifying the type of guide to use for continuous scales. Default is 'legend'.
+#' @param fallback_font Adds a fallback font of 'sans' in case user does not have required font.
+#'
+#' @return A list containing the ggplot2 theme, fill scale, and colour scale.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' # Example with continuous theme
+#' ggplot(mtcars, aes(x = wt, y = mpg, colour = qsec)) +
+#'   geom_point() +
+#'   theme_share(scale_type = "continuous")
+#'
+#' # Example with discrete theme
+#' ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, colour = Species)) +
+#'   geom_point() +
+#'   theme_share(scale_type = "discrete")
+#'   }
+dr_theme_share <- function(scale_type = c("discrete", "continuous"),
+                        index = NULL,
+                        direction = 1,
+                        guide = 'legend',
+                        fallback_font = "sans") {
+
+  scale_type <- match.arg(scale_type)
+
+  if (identical(Sys.getenv("R_CHECK_ENVIRON"), "true")) {
+    font_family <- fallback_font
+  } else {
+    font_family <- "Neue Haas Grotesk Text Pro 55 Roman"
+  }
+
+  if (scale_type == "continuous") {
+
+    # The continuous code block from theme_share_continuous
+    return(theme_share_continuous(index, direction, guide, font_family = font_family))
+
+  } else if (scale_type == "discrete") {
+
+    # The discrete code block from theme_share_discrete
+    return(theme_share_discrete(index, font_family = font_family))
+
+  } else {
+    stop("Invalid scale_type argument. Must be either 'continuous' or 'discrete'.")
+  }
+
+}
+
+
+#' theme_share_continuous
+#'
+#' Adds SHARE colours and font to continuous plot.
+#' @param index Choose palettes colours by index by setting index equal to a character vector e.g. c(1,2,3) or c(1:3)
+#' @param direction The direction of the colours in the scale. Set to -1 to reverse them.
+#' @param guide The type of legend. Use "colourbar", "legend" or FALSE.
+#'
+#' @keywords internal
+theme_share_continuous <- function(index = NULL, direction = 1, guide = 'legend', font_family = "Neue Haas Grotesk Text Pro 55 Roman"){
+
+  values <- c("#0f50d2",
+                       "#7800c6",
+                       "#d80a83",
+                       "#ffb600",
+                       "#ff4e00",
+                       "#bb7fe2",
+                       "#ffda74",
+                       "#ffa67f",
+                       "#eb84c1")
+
+  if (is.numeric(index)){
+    values <- palette[index]
+  } else {
+    values <- c("#ffb600",
+                         "#d80a83",
+                         "#0f50d2")
+  }
+
+  if (direction == 1){
+    fill_scale <- ggplot2::scale_fill_gradientn(colours = values,
+                                                labels = scales::comma,
+                                                breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                guide = guide)
+    colour_scale <- ggplot2::scale_colour_gradientn(colours = values,
+                                                    labels = scales::comma,
+                                                    breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                    guide = guide)
+  } else if (direction == -1){
+    fill_scale <- ggplot2::scale_fill_gradientn(colours = rev(values),
+                                                labels = scales::comma,
+                                                breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                guide = guide)
+    colour_scale <- ggplot2::scale_colour_gradientn(colours = rev(values),
+                                                    labels = scales::comma,
+                                                    breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                    guide = guide)
+  }
+
+  list(ggplot2::theme_minimal(base_family = font_family),
+       fill_scale,
+       colour_scale)
+
+}
+
+
+#' theme_share_discrete
+#'
+#' Adds SHARE colours and font to discrete plot.
+#' @param index Choose palettes colours by index by setting index equal to a character vector e.g. c(1,2,3) or c(1:3)
+#' @keywords internal
+theme_share_discrete <- function(index = NULL, font_family = "Neue Haas Grotesk Text Pro 55 Roman"){
+
+  values <- c("#0f50d2",
+                       "#7800c6",
+                       "#d80a83",
+                       "#ffb600",
+                       "#ff4e00",
+                       "#bb7fe2",
+                       "#ffda74",
+                       "#ffa67f",
+                       "#eb84c1")
 
   if (is.numeric(index)){
     values <- values[index]
