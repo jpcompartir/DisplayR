39bcd8cfb82cf62d73ae75462b7fa713d73f7d7c
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-05-24 15:52:57 +0100

    add samy theme

--- R/themes.R
+++ R/themes.R
@@ -23,18 +23,18 @@ dr_theme_black <- function(){
 
 }
 
-#' Apply Microsoft Themed Color Scales and Aesthetics to ggplot2 Plots
+#' Apply Microsoft Themed Colour Scales and Aesthetics to ggplot2 Plots
 #'
-#' This function provides a convenient way to apply Microsoft themed color scales and aesthetics to ggplot2 plots.
+#' This function provides a convenient way to apply Microsoft themed colour scales and aesthetics to ggplot2 plots.
 #' The user can specify whether they want a continuous or discrete theme by providing the `scale_type` argument.
 #'
 #' @param scale_type A character string specifying the type of scale, either "continuous" or "discrete". Default is "discrete".
-#' @param index An optional numeric index or vector of indices to select specific colors from the Microsoft color palette.
-#' @param direction An optional numeric value (1 or -1) specifying the direction of the color gradient for continuous scales. Default is 1.
+#' @param index An optional numeric index or vector of indices to select specific colours from the Microsoft color palette.
+#' @param direction An optional numeric value (1 or -1) specifying the direction of the colour gradient for continuous scales. Default is 1.
 #' @param guide An optional character string specifying the type of guide to use for continuous scales. Default is 'legend'.
 #' @param fallback_font Adds a fallback font of 'sans' in case user does not have required font.
 #'
-#' @return A list containing the ggplot2 theme, fill scale, and color scale.
+#' @return A list containing the ggplot2 theme, fill scale, and colour scale.
 #' @export
 #'
 #' @examples
@@ -51,7 +51,7 @@ dr_theme_black <- function(){
 #'   geom_point() +
 #'   theme_microsoft(scale_type = "discrete")
 #'   }
-theme_microsoft <- function(scale_type = c("discrete", "continuous"),
+dr_theme_microsoft <- function(scale_type = c("discrete", "continuous"),
                             index = NULL,
                             direction = 1,
                             guide = 'legend',
@@ -184,3 +184,149 @@ theme_microsoft_discrete <- function(index = NULL, font_family = 'Segoe UI Regul
 
 }
 
+
+#' Apply SAMY Themed Colour Scales and Aesthetics to ggplot2 Plots
+#'
+#' This function provides a convenient way to apply Microsoft themed color scales and aesthetics to ggplot2 plots.
+#' The user can specify whether they want a continuous or discrete theme by providing the `scale_type` argument.
+#'
+#' @param scale_type A character string specifying the type of scale, either "continuous" or "discrete". Default is "discrete".
+#' @param index An optional numeric index or vector of indices to select specific colors from the SAMY color palette.
+#' @param direction An optional numeric value (1 or -1) specifying the direction of the color gradient for continuous scales. Default is 1.
+#' @param guide An optional character string specifying the type of guide to use for continuous scales. Default is 'legend'.
+#' @param fallback_font Adds a fallback font of 'sans' in case user does not have required font.
+#'
+#' @return A list containing the ggplot2 theme, fill scale, and color scale.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' library(ggplot2)
+#'
+#' # Example with continuous theme
+#' ggplot(mtcars, aes(x = wt, y = mpg, color = qsec)) +
+#'   geom_point() +
+#'   theme_samy(scale_type = "continuous")
+#'
+#' # Example with discrete theme
+#' ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+#'   geom_point() +
+#'   theme_samy(scale_type = "discrete")
+#'}
+dr_theme_samy <- function(scale_type = c("discrete", "continuous"),
+                       index = NULL,
+                       direction = 1,
+                       guide = 'legend',
+                       fallback_font = "sans"){
+  scale_type <- match.arg(scale_type)
+
+  if (identical(Sys.getenv("R_CHECK_ENVIRON"), "true")) {
+    font_family <- fallback_font
+  } else {
+    font_family <- 'Montserrat Regular'
+  }
+
+
+  if (scale_type == "continuous") {
+
+    # The continuous code block from theme_samy_continuous
+    return(theme_samy_continuous(index, direction, guide, font_family = font_family))
+
+  } else if (scale_type == "discrete") {
+
+    # The discrete code block from theme_samy_discrete
+    return(theme_samy_discrete(index, font_family = font_family))
+
+  } else {
+    stop("Invalid scale_type argument. Must be either 'continuous' or 'discrete'.")
+  }
+
+}
+
+
+#' theme_samy_continuous
+#'
+#' Adds Samy colours and font to continuous plot.
+#' @param index Choose palettes colours by index by setting index equal to a character vector e.g. c(1,2,3) or c(1:3)
+#' @param direction The direction of the colours in the scale. Set to -1 to reverse them.
+#' @param guide The type of legend. Use "colourbar", "legend" or FALSE.
+#'
+#' @keywords internal
+theme_samy_continuous <- function(index = NULL, direction = 1, guide = 'legend',  font_family = 'Montserrat Regular'){
+
+  palette <- c("#3fbbbb",
+                        "#ff5b51",
+                        "#ffcf0e",
+                        "#7b7b7c",
+                        "#c1e5e2",
+                        "#f15c54",
+                        "#ffe9ab",
+                        "#aaabaa",
+                        "#f8aa95",
+                        "#dfe0e1")
+
+  if (is.numeric(index)){
+    values <- palette[index]
+  } else {
+    values <- c("#ffcf0e",
+                         "#f15c54",
+                         "#3fbbbb")
+  }
+
+  if (direction == 1){
+    fill_scale <- ggplot2::scale_fill_gradientn(colours = values,
+                                                labels = scales::comma,
+                                                breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                guide = guide)
+    colour_scale <- ggplot2::scale_colour_gradientn(colours = values,
+                                                    labels = scales::comma,
+                                                    breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                    guide = guide)
+  } else if (direction == -1){
+    fill_scale <- ggplot2::scale_fill_gradientn(colours = rev(values),
+                                                labels = scales::comma,
+                                                breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                guide = guide)
+    colour_scale <- ggplot2::scale_colour_gradientn(colours = rev(values),
+                                                    labels = scales::comma,
+                                                    breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                    guide = guide)
+  }
+
+  list(ggplot2::theme_minimal(base_family = font_family),
+       fill_scale,
+       colour_scale)
+
+}
+
+
+
+
+#' theme_samy_discrete
+#'
+#' Adds Samy colours and font to discrete plot.
+#' @param index Choose palettes colours by index by setting index equal to a character vector e.g. c(1,2,3) or c(1:3)
+#' @keywords internal
+theme_samy_discrete <- function(index = NULL, font_family = 'Montserrat Regular'){
+
+  values <- c("#3fbbbb",
+                       "#ff5b51",
+                       "#ffcf0e",
+                       "#7b7b7c",
+                       "#c1e5e2",
+                       "#f15c54",
+                       "#ffe9ab",
+                       "#aaabaa",
+                       "#f8aa95",
+                       "#dfe0e1")
+
+  if (is.numeric(index)){
+    values <- values[index]
+  }
+
+  list(ggplot2::theme_minimal(base_family = font_family),
+       ggplot2::scale_discrete_manual(aesthetics = c('fill', 'colour'),
+                                      values = values))
+
+}
