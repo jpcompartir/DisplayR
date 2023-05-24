b2c4fcf216ee0fe950503c24114f5cfb18e647a6
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-05-24 15:51:14 +0100

    add microsoft theme

--- R/themes.R
+++ R/themes.R
@@ -4,7 +4,7 @@
 #' @export
 dr_theme_black <- function(){
 
-  output = ggplot2::theme(plot.background = ggplot2::element_rect(fill = "black", color = "black"),
+  output <- ggplot2::theme(plot.background = ggplot2::element_rect(fill = "black", color = "black"),
                           strip.text = ggplot2::element_text(color = "white"),
                           strip.background = ggplot2::element_rect(fill = "black"),
                           panel.background = ggplot2::element_rect(fill = 'black'),
@@ -19,7 +19,168 @@ dr_theme_black <- function(){
                           panel.grid.major = ggplot2::element_blank(),
                           panel.grid.minor = ggplot2::element_blank())
 
-  output
+  return(output)
+
+}
+
+#' Apply Microsoft Themed Color Scales and Aesthetics to ggplot2 Plots
+#'
+#' This function provides a convenient way to apply Microsoft themed color scales and aesthetics to ggplot2 plots.
+#' The user can specify whether they want a continuous or discrete theme by providing the `scale_type` argument.
+#'
+#' @param scale_type A character string specifying the type of scale, either "continuous" or "discrete". Default is "discrete".
+#' @param index An optional numeric index or vector of indices to select specific colors from the Microsoft color palette.
+#' @param direction An optional numeric value (1 or -1) specifying the direction of the color gradient for continuous scales. Default is 1.
+#' @param guide An optional character string specifying the type of guide to use for continuous scales. Default is 'legend'.
+#' @param fallback_font Adds a fallback font of 'sans' in case user does not have required font.
+#'
+#' @return A list containing the ggplot2 theme, fill scale, and color scale.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' # Example with continuous theme
+#' ggplot(mtcars, aes(x = wt, y = mpg, color = qsec)) +
+#'   geom_point() +
+#'   theme_microsoft(scale_type = "continuous")
+#'
+#' # Example with discrete theme
+#' ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+#'   geom_point() +
+#'   theme_microsoft(scale_type = "discrete")
+#'   }
+theme_microsoft <- function(scale_type = c("discrete", "continuous"),
+                            index = NULL,
+                            direction = 1,
+                            guide = 'legend',
+                            fallback_font = "sans") {
+
+  scale_type <- match.arg(scale_type)
+
+  # Use the fallback font if we're in the RMarkdown check environment
+  if (identical(Sys.getenv("R_CHECK_ENVIRON"), "true")) {
+    font_family <- fallback_font
+  } else {
+    font_family <- "Segoe UI Regular"
+  }
+
+  #Return the continuous function if necessary and if not discrete
+  if (scale_type == "continuous") {
+
+    # The continuous code block from theme_microsoft_continuous
+    return(theme_microsoft_continuous(index, direction, guide, font_family = font_family))
+
+  } else if (scale_type == "discrete") {
+
+    # The discrete code block from theme_microsoft_discrete
+    return(theme_microsoft_discrete(index, font_family = font_family))
+
+  } else {
+    stop("Invalid scale_type argument. Must be either 'continuous' or 'discrete'.")
+  }
+
+}
+
+#' theme_microsoft_continous
+#'
+#' Adds Microsoft colours and font to continous plot.
+#' @param index Choose palettes colours by index by setting index equal to a character vector e.g. c(1,2,3) or c(1:3)
+#' @param direction The direction of the colours in the scale. Set to -1 to reverse them.
+#' @param guide The type of legend. Use "colourbar", "legend" or FALSE.
+
+#' @keywords internal
+theme_microsoft_continuous <- function(index = NULL, direction = 1, guide = 'legend', font_family = "Segoe UI Regular"){
+
+  palette = c(
+    "#D83B01", # Orange
+             "#FFB900", # Yellow
+             "#107C10", # Green
+             "#008575", # Teal
+             "#0078D4", # Blue
+             "#8661C5", # Purple,
+             "#FF9349", # Light Orange
+             "#243a5e", # Dark Blue
+             "#9BF00B", # Light Green,
+             "#30E5D0", # Light Teal,
+             "#50E6FF", # Light Blue
+             "#D59DFF", # Light Purple
+             "#6b2929", # Dark Orange
+             "#6a4b16", # Dark Yellow
+             "#054b17", # Dark Green
+             "#274b47", # Dark Teal
+             "#3b2e58" # Dark Purple
+  )
+
+  if (is.numeric(index)){
+    values <- palette[index]
+  } else {
+    values <- c("#ffb900",
+                         "#107c10",
+                         "#0078d4")
+  }
+
+  if (direction == 1){
+    fill_scale <- ggplot2::scale_fill_gradientn(colours = values,
+                                                labels = scales::comma,
+                                                breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                guide = guide)
+    colour_scale <- ggplot2::scale_colour_gradientn(colours = values,
+                                                    labels = scales::comma,
+                                                    breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                    guide = guide)
+  } else if (direction == -1){
+    fill_scale <- ggplot2::scale_fill_gradientn(colours = rev(values),
+                                                labels = scales::comma,
+                                                breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                guide = guide)
+    colour_scale <- ggplot2::scale_colour_gradientn(colours = rev(values),
+                                                    labels = scales::comma,
+                                                    breaks = function(x) round(stats::quantile(x, seq(0, 1, 0.25))),
+                                                    guide = guide)
+  }
+
+  list(ggplot2::theme_minimal(base_family = font_family),
+       fill_scale,
+       colour_scale)
+
+}
+
+#' theme_microsoft_discrete
+#'
+#' Adds Microsoft colours and font to discrete plot.
+#' @param index Choose palettes colours by index by setting index equal to a character vector e.g. c(1,2,3) or c(1:3)
+#' @keywords internal
+theme_microsoft_discrete <- function(index = NULL, font_family = 'Segoe UI Regular'){
+
+  values = c(
+    "#D83B01", # Orange
+             "#FFB900", # Yellow
+             "#107C10", # Green
+             "#008575", # Teal
+             "#0078D4", # Blue
+             "#8661C5", # Purple,
+             "#FF9349", # Light Orange
+             "#243a5e", # Dark Blue
+             "#9BF00B", # Light Green,
+             "#30E5D0", # Light Teal,
+             "#50E6FF", # Light Blue
+             "#D59DFF", # Light Purple
+             "#6b2929", # Dark Orange
+             "#6a4b16", # Dark Yellow
+             "#054b17", # Dark Green
+             "#274b47", # Dark Teal
+             "#3b2e58" # Dark Purple
+  )
+
+  if (is.numeric(index)){
+    values <- values[index]
+  }
+
+  list(ggplot2::theme_minimal(base_family = 'Segoe UI Regular'),
+       ggplot2::scale_discrete_manual(aesthetics = c('fill', 'colour'),
+                                      values = values))
 
 }
 
