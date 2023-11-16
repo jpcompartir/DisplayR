f2e8c1ff85b6bdcd80d3c1d4623f489cf15bcd52
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-11-16 11:12:37 +0000

    add test_plot() with the test boilerplate generation.

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,10 @@
 globals <- utils::globalVariables(c("plot_date", "n", "percent", "fp_text", ":=", "volume", "Sentiment x Time", "Neutral", "Volume x Time", "sentiment", "df", "group", "positive", "neutral", "negative", "Positive", "Negative", "Volume", "group_n", "percent_character", "topic", ".total", "palette", "pdfFonts"))
+
+
+test_plot <- function() {plot <-
+  plot <- ggplot2::ggplot(data = iris,
+                  ggplot2::aes(x = Sepal.Length, y = Sepal.Width, colour = Sepal.Width)) +
+  ggplot2::geom_point()
+
+  return(plot)
+}
