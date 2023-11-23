65729d8d8141ec34e0c1a72318bb268c9ede7af2
jamiehshare <jamie.hudson@sharecreative.com>
2023-11-23 09:18:02 +0000

    Update plots_distribution.R
    Reworked bandwidth to be smoothness, for easier understanding for the user. Also changed the limits of this from 0-1 to just >0 to more accurately represent the measure

--- R/plots_distribution.R
+++ R/plots_distribution.R
@@ -1,22 +1,33 @@
+#' Quickly create a raincloud plot
+#'
 #' Create a raincloud plot to provide maximal statistical information while preserving the desired ‘inference at a glance’ nature of barplots and other similar visualisation devices.
 #'
 #' @param data A data frame that includes the grouping and continuous variable.
 #' @param grouping_variable The variable we want to map colours to.
 #' @param continuous_variable The continious variable we want to be displaying the distribution of.
-#' @param bandwidth A measure, ranging from 0-1, representing the smoothness of the density plot (i.e. how closely you want the density to match the distribution). Default is 0.3, but too high values can underrepresent the data, and too low values can overfit the density.
+#' @param smoothness A measure, which must be > 0, representing the smoothness of the density plot (i.e. how closely you want the density to match the distribution). Default is 0.3, but too high values can oversmooth the data, and too low values can cause undersmoothing. Generally it is recommended to stick with values between 0.3-1.
 #'
-#' @return
+#' @return A ggplot2 object
 #' @export
 #'
 #' @examples
+#' ## Bandwidth default value of 0.3
 #' iris %>%
-#' dr_plot_raincloud(grouping_variable = Species, independent_variable = Sepal.Length)
+#'   dr_plot_raincloud(grouping_variable = Species,
+#'                     continuous_variable = Sepal.Length,
+#'                     smoothness = 0.3)
+#'
+#' ## Bandwidth changed to 0.8
+#' iris %>%
+#'   dr_plot_raincloud(grouping_variable = Species,
+#'                     continuous_variable = Sepal.Length,
+#'                     smoothness = 0.8)
 
-dr_plot_raincloud <- function(data, grouping_variable, continuous_variable, bandwidth = 0.3) {
+dr_plot_raincloud <- function(data, grouping_variable, continuous_variable, smoothness = 0.3) {
 
-  stopifnot(is.numeric(bandwidth),
-            bandwidth > 0,
-            bandwidth < 1)
+  stopifnot(is.numeric(smoothness),
+            smoothness > 0,
+            smoothness < 1)
 
   # Get var for tidy eval
   group_sym <- rlang::ensym(grouping_variable)
@@ -38,7 +49,7 @@ dr_plot_raincloud <- function(data, grouping_variable, continuous_variable, band
       outlier.shape = NA
     ) +
     ggdist::stat_halfeye(
-      adjust = bandwidth,
+      adjust = smoothness,
       width = 0.5,
       color = NA, ## remove slab interval
       position = ggplot2::position_nudge(x = 0.15)
@@ -48,7 +59,7 @@ dr_plot_raincloud <- function(data, grouping_variable, continuous_variable, band
       range_scale = 0.3,
       alpha = 0.5
     ) +
-    DisplayR:::theme_boilerplate()
+    theme_boilerplate()
 
   return(plot_raincloud)
 }
