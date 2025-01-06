540d98e7c1af40b9b04747dcdd261c2c44603687
jpcompartir <jp1689@my.bristol.ac.uk>
2025-01-06 13:22:20 +0000

    add basic tests for brands and doc for brands funcs

--- R/brands.R
+++ R/brands.R
@@ -1,3 +1,12 @@
+#' View which brands have styling options available for them
+#'
+#' @description Check which brands a _brand.yml and an optional 'logo.png' have been uploaded to DisplayR for.
+#'
+#' @returns A list of folder names relating to brands/
+#' @export
+#'
+#' @examples
+#' brands <- dr_list_brands()
 dr_list_brands <- function(){
  brands_path <- system.file("brands", package = "DisplayR")
 
@@ -12,6 +21,20 @@ dr_list_brands <- function(){
   return(brands[has_brand_yml])
 }
 
+#' Add brand styling to your project
+#'
+#' @description Add a _brand.yml and a logo.png to your project from DisplayR's inst/brands folder. If you need a new brand to be added, contact the Data Science Team
+#' @param brand Name of the brand you wish to load, as it appears in `dr_list_brands()`
+#' @param directory  The directory you want the brand information to be added to, as a default it uses the current working directory
+#'
+#' @returns Invisible
+#' @export
+#'
+#' @examples
+#'
+#' brands <- dr_list_brands()
+#' dr_add_brand("shareds")
+#'
 dr_add_brand <- function(brand, directory = getwd()) {
 
   brands_path <- system.file("brands", package = "DisplayR")
